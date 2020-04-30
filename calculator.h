44a611996e5f6298d0cda3cbc0de13207d819b6e
fabbitox <64521259+fabbitox@users.noreply.github.com>
2020-04-30 22:31:13 +0900

    Delete calculator.h

--- calculator.h
+++
@@ -1,9 +0,0 @@
-#ifndef _CALCULATOR_H_
-#define _CALCULATOR_H_
-
-#include <stdlib.h>
-
-Addfunc(double val1, double val2);
-Minusfunc(double val1, double val2);
-Multifunc(double val1, double val2);
-Dividefunc(double val1, double val2);
